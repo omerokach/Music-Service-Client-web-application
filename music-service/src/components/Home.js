21baf4e3f4384bbb5f164de960f04d930cdf5f02
omer rokach <77382841+omerokach@users.noreply.github.com>
2021-04-05 22:21:37 +0300

    build playlist

--- music-service/src/components/Home.js
+++ music-service/src/components/Home.js
@@ -1,103 +1,22 @@
 import React, { useState } from "react";
 import data from "../musiceStorage/musicLab.json";
-//retrive top 5 songs by views
-const findTopFiveSongs = () => {
-  const topFiveSongs = [];
-  const topFiveViews = [];
-  const allSongsViews = [];
-  const allSongs = [];
-  data.forEach((artist) => {
-    artist.albumArray.forEach((album) => {
-      album.songs.forEach((song) => {
-        allSongs.push(song);
-        allSongsViews.push(song.statistics.viewCount);
-      });
-    });
-  });
-  //sorting all songs views
-  allSongsViews.sort((a, b) => a - b);
-  for (let i = 0; i < 5; i++) {
-    topFiveViews.push(allSongsViews.pop());
-  }
-  //taking only the first 5
-  allSongs.forEach((song) => {
-    for (let i = 0; i < 5; i++) {
-      if (song.statistics.viewCount === topFiveViews[i]) {
-        topFiveSongs.push(song);
-      }
-    }
-  });
-  return topFiveSongs;
-};
+import {
+  findTopFiveSongs,
+  findTopFiveArtists,
+  findTopFiveAlbums,
+  findTopFivePlayList,
+} from "../utils";
 
-//retrive top 5 artists by views
-const findTopFiveArtists = () => {
-  const topFiveArtists = [];
-  const topFiveViews = [];
-  const allArtistsViews = [];
-  const allArtists = [];
-  data.forEach((artist) => {
-    allArtists.push(artist);
-    allArtistsViews.push(artist.views);
-  });
-  //sorting all artist views
-  allArtistsViews.sort((a, b) => a - b);
-  for (let i = 0; i < 5; i++) {
-    topFiveViews.push(allArtistsViews.pop());
-  }
-  //taking only the first 5
-  allArtists.forEach((artist) => {
-    for (let i = 0; i < 5; i++) {
-      if (artist.views === topFiveViews[i]) {
-        topFiveArtists.push(artist);
-      }
-    }
-  });
-  return topFiveArtists;
-};
-
-//retrive top 5 albums by views
-const findTopFiveAlbums = () => {
-  const topFiveAlbums = [];
-  const topFiveViews = [];
-  const allAlbumsViews = [];
-  const allAlbums = [];
-  let sumViews = 0;
-  data.forEach((artist) => {
-    artist.albumArray.forEach((album) => {
-      album.songs.forEach((song) => {
-        sumViews += Number(song.statistics.viewCount);
-      });
-      allAlbumsViews.push(sumViews);
-      allAlbums.push({ album: album, sumViews: sumViews });
-    });
-  });
-  //sorting all artist views
-  allAlbumsViews.sort((a, b) => a - b);
-  for (let i = 0; i < 5; i++) {
-    topFiveViews.push(allAlbumsViews.pop());
-  }
-  console.log(allAlbums);
-  //taking only the first 5
-  allAlbums.forEach((album) => {
-    for (let i = 0; i < 5; i++) {
-      if (album.sumViews === topFiveViews[i]) {
-        topFiveAlbums.push(album);
-      }
-    }
-  });
-  return topFiveAlbums;
-};
 const topFiveArtists = findTopFiveArtists();
 const topFiveSongs = findTopFiveSongs();
 const topFiveAlbums = findTopFiveAlbums();
-
+const topfivePlayList = findTopFivePlayList();
 function Home(props) {
   console.log(topFiveArtists);
   console.log(topFiveSongs);
   console.log(topFiveAlbums);
+  console.log(topfivePlayList);
   const [musicLibray, setMusicLibrary] = useState(data);
-  console.log(musicLibray);
   return (
     <div>
       <div id="top-artist"></div>
--- music-service/src/utils.js
+++ music-service/src/utils.js
@@ -1,87 +1,125 @@
-
-const funcs = {
-    //retrive top 5 songs by views
-    findTopFiveSongs: function (data){
-      const topFiveSongs = [];
-      const topFiveViews = [];
-      const allSongsViews = [];
-      const allSongs = [];
-      data.map((artist) => {
-        artist.albumArray.map((album) => {
-          album.songs.map((song) => {
-            allSongs.push(song);
-            allSongsViews.push(song.statistics.viewCount);
-          });
-        });
+import data from "./musiceStorage/musicLab.json";
+//retrive top 5 songs by views
+export function findTopFiveSongs() {
+  const topFiveSongs = [];
+  const topFiveViews = [];
+  const allSongsViews = [];
+  const allSongs = [];
+  data.forEach((artist) => {
+    artist.albumArray.forEach((album) => {
+      album.songs.forEach((song) => {
+        allSongs.push(song);
+        allSongsViews.push(song.statistics.viewCount);
       });
-      //sorting all songs views
-      allSongsViews.sort((a, b) => a - b);
-      for (let i = 0; i < 5; i++) {
-        topFiveViews.push(allSongsViews.pop());
+    });
+  });
+  //sorting all songs views
+  allSongsViews.sort((a, b) => a - b);
+  for (let i = 0; i < 5; i++) {
+    topFiveViews.push(allSongsViews.pop());
+  }
+  //taking only the first 5
+  allSongs.forEach((song) => {
+    for (let i = 0; i < 5; i++) {
+      if (song.statistics.viewCount === topFiveViews[i]) {
+        topFiveSongs.push(song);
       }
-      //taking only the first 5
-      allSongs.map((song) => {
-        for (let i = 0; i < 5; i++) {
-          if (song.statistics.viewCount === topFiveViews[i]) {
-            topFiveSongs.push(song);
-          }
-        }
-      });
-      return topFiveSongs;
-    },
-    
-    //retrive top 5 artists by views
-    findTopFiveArtists: function (data) {
-      const topFiveArtists = [];
-      const topFiveViews = [];
-      const allArtistsViews = [];
-      const allArtists = [];
-      data.map((artist) => {
-        allArtists.push(artist);
-        allArtistsViews.push(artist.views);
-      });
-      //sorting all artist views
-      allArtistsViews.sort((a, b) => a - b);
-      for (let i = 0; i < 5; i++) {
-        topFiveViews.push(allArtistsViews.pop());
-      }
-      //taking only the first 5
-      allArtists.map((artist) => {
-        for (let i = 0; i < 5; i++) {
-          if (artist.views === topFiveViews[i]) {
-            topFiveArtists.push(artist);
-          }
-        }
-      });
-      return topFiveArtists;
-    },
-    
-    //retrive top 5 albums by views
-    findTopFiveAlbums: function (data) {
-      const topFiveAlbums = [];
-      const topFiveViews = [];
-      const allAAlbumsViews = [];
-      const allAlbums = [];
-      data.map((artist) => {
-        allAlbums.push(...artist.albumArray);
-        console.log(allAlbums);
-        // allAAlbumsViews.push(artist.views);
-      });
-      //sorting all artist views
-      allAAlbumsViews.sort((a, b) => a - b);
-      for (let i = 0; i < 5; i++) {
-        topFiveViews.push(allAAlbumsViews.pop());
-      }
-      //taking only the first 5
-      allAlbums.map((artist) => {
-        for (let i = 0; i < 5; i++) {
-          if (artist.views === topFiveViews[i]) {
-            topFiveAlbums.push(artist);
-          }
-        }
-      });
-      return topFiveAlbums;
     }
+  });
+  return topFiveSongs;
 }
 
-export default funcs;
+//retrive top 5 artists by views
+export function findTopFiveArtists() {
+  const topFiveArtists = [];
+  const topFiveViews = [];
+  const allArtistsViews = [];
+  const allArtists = [];
+  data.forEach((artist) => {
+    allArtists.push(artist);
+    allArtistsViews.push(artist.views);
+  });
+  //sorting all artist views
+  allArtistsViews.sort((a, b) => a - b);
+  for (let i = 0; i < 5; i++) {
+    topFiveViews.push(allArtistsViews.pop());
+  }
+  //taking only the first 5
+  allArtists.forEach((artist) => {
+    for (let i = 0; i < 5; i++) {
+      if (artist.views === topFiveViews[i]) {
+        topFiveArtists.push(artist);
+      }
+    }
+  });
+  return topFiveArtists;
+}
+
+//retrive top 5 albums by views
+export function findTopFiveAlbums() {
+  const topFiveAlbums = [];
+  const topFiveViews = [];
+  const allAlbumsViews = [];
+  const allAlbums = [];
+  let sumViews = 0;
+  data.forEach((artist) => {
+    artist.albumArray.forEach((album) => {
+      album.songs.forEach((song) => {
+        sumViews += Number(song.statistics.viewCount);
+      });
+      allAlbumsViews.push(sumViews);
+      allAlbums.push({ album: album, sumViews: sumViews });
+    });
+  });
+  //sorting all artist views
+  allAlbumsViews.sort((a, b) => a - b);
+  for (let i = 0; i < 5; i++) {
+    topFiveViews.push(allAlbumsViews.pop());
+  }
+  //taking only the first 5
+  allAlbums.forEach((album) => {
+    for (let i = 0; i < 5; i++) {
+      if (album.sumViews === topFiveViews[i]) {
+        topFiveAlbums.push(album);
+      }
+    }
+  });
+  return topFiveAlbums;
+}
+
+//retrive top 5 playlist by views
+export function findTopFivePlayList() {
+  const topViews = [];
+  const allSongsViews = [];
+  const allSongs = [];
+  data.forEach((artist) => {
+    artist.albumArray.forEach((album) => {
+      album.songs.forEach((song) => {
+        allSongs.push(song);
+        allSongsViews.push(song.statistics.viewCount);
+      });
+    });
+  });
+  //sorting all songs views
+  allSongsViews.sort((a, b) => a - b);
+  for (let i = 0; i < 125; i++) {
+    topViews.push(allSongsViews.pop());
+  }
+  let topFivePlayList = [];
+  let playList = [];
+  //taking only the first 5
+  allSongs.forEach((song) => {
+    for (let i = 0; i < 125; i++) {
+      if (song.statistics.viewCount === topViews[i]) {
+        playList.push(song);
+        if (playList.length === 25) {
+          topFivePlayList.push(playList);
+          playList = [];
+        }
+      }
+      if (topFivePlayList.length === 5) break;
+    }
+  });
+  console.log("topFivePlayList", topFivePlayList);
+  return topFivePlayList;
+}
