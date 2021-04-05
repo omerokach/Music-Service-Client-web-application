dab3f20919e07500a8aea6f41fef7e2f7f730982
omer rokach <77382841+omerokach@users.noreply.github.com>
2021-04-05 22:48:12 +0300

    routes from home

--- music-service/src/App.js
+++ music-service/src/App.js
@@ -1,25 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/Home";
-import PlayList from './components/PlayList'
-import Song from './components/Song';
-import Artist from './components/Artist'
-import Album from './components/Album'
+import PlayList from "./components/PlayList";
+import Song from "./components/Song";
+import Artist from "./components/Artist";
+import Album from "./components/Album";
 import { Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 
+import {
+  findTopFiveSongs,
+  findTopFiveArtists,
+  findTopFiveAlbums,
+  findTopFivePlayList,
+} from "./utils";
+const topFiveArtists = findTopFiveArtists();
+const topFiveSongs = findTopFiveSongs();
+const topFiveAlbums = findTopFiveAlbums();
+const topfivePlayList = findTopFivePlayList();
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Route path='/' component={Home} />
-      <Route path='/playlist/:id' component={PlayList} />
-      <Route path='/artist/:id' component={Artist} />
-      <Route path='/album/:id' component={Album} />
-      <Route path='/song/:id/' component={Song} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/playlist/:id" component={PlayList} />
+        <Route exact path="/artist/:id" component={Artist} />
+        <Route exact path="/album/:id" component={Album} />
+        <Route exact path="/song/:id/" component={Song} />
       </BrowserRouter>
-    
     </div>
   );
 }
--- music-service/src/components/Home.js
+++ music-service/src/components/Home.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import { BrowserRouter, Link } from "react-router-dom";
-import data from "../musiceStorage/musicLab.json";
 import {
   findTopFiveSongs,
   findTopFiveArtists,
   findTopFiveAlbums,
   findTopFivePlayList,
 } from "../utils";
+import PlayList from "./PlayList";
+import Song from "./Song";
+import Artist from "./Artist";
+import Album from "./Album";
+import { Route } from "react-router";
 
 const topFiveArtists = findTopFiveArtists();
 const topFiveSongs = findTopFiveSongs();
@@ -17,11 +21,11 @@ function Home(props) {
   console.log(topFiveSongs);
   console.log(topFiveAlbums);
   console.log(topfivePlayList);
-  const [musicLibray, setMusicLibrary] = useState(data);
+
   return (
     <div>
       <div id="top-artist">
-        <Link></Link>
+        <Link to=""></Link>
       </div>
       <div id="top-albums">
         <Link></Link>
@@ -32,6 +36,12 @@ function Home(props) {
       <div id="top-playlist">
         <Link></Link>
       </div>
+      <BrowserRouter>
+        <Route exact path="/playlist/:id" component={PlayList} />
+        <Route exact path="/artist/:id" component={Artist} />
+        <Route exact path="/album/:id" component={Album} />
+        <Route exact path="/song/:id/" component={Song} />
+      </BrowserRouter>
     </div>
   );
 }
